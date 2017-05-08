c607766847d7b4be069b8f9207bcd9d1e3cadb54
Juan Patricio Di Bacco <jpdibacco@gmail.com>
2017-05-08 00:19:15 -0300

    adding minesweeper logic dependency npm, adding board logic

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,70 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div v-bind:class="hideOpts">
+    <img src="./assets/logo.png" width="128" height="128">
     <h1>{{msg}}</h1>
     <h2>Set options bellow:</h2>
     <div id="inputbox">
-      <label># Cols: <input type="number" name="cols" value=""></label>
+      <label># Cols: <input type="number" name="cols" value="" required="You must set a #" min="0" max="100" step="5" v-on:input="nCols($event)"></label>
       <br><br>
-      <label># Rows: <input type="number" name="rows" value=""></label>
+      <label># Rows: <input type="number" name="rows" value="" required="You must set a #" min="0" max="100" step="5" v-on:input="nRows($event)"></label>
       <br><br>
-      <label># Mines: <input type="number" name="mines" value=""></label>
+      <label># Mines: <input type="number" name="mines" value="" required="You must set a #" min="0" max="100" step="1" v-on:input="nMines($event)"></label>
       <br><br>
-      <button>Start</button>
+      <button v-on:click="newGame">Start</button>
     </div>
   </div>
+    <board></board>
+</div>
 </template>
 
 <script>
+import Board from './board.vue';
+var cols, rows, mines;
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Mine Sweeper Demo'
+      msg: 'Mine Sweeper Demo',
+      hideOpts:''
     }
+  },
+  methods:
+    {
+    newGame() {
+      this.hideOpts='hideMe';
+      this.$emit('newGame');
+      this.$emit('setDiff', 1);
+      //this.$root.$emit('newGame');
+      //this.startGame = event.target.value;
+      console.log('emmiting bus...');
+    },
+    nCols(event){
+      cols = event.target.value;
+      localStorage.setItem("cols", cols);
+      console.log("# cols: "+ cols)
+    },
+    nRows(event){
+      rows = event.target.value;
+      localStorage.setItem("rows", rows);
+      console.log("# rows: " + rows)
+    },
+    nMines(event){
+      mines = event.target.value;
+      localStorage.setItem("mines", mines);
+      console.log('# mines: '+ mines)
+    }
+  },
+  components: {
+    Board
   }
 }
 </script>
 
 <style lang="scss">
+.hideMe{
+  display: none;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/board.vue
@@ -0,0 +1,42 @@
+<template>
+<div v-bind:class="showGame">
+    <p>This is an import for game logic</p>
+    <ol id="Board">
+        <li class="row"></li>
+        <li>#Cols:{{nCols}}</li>
+        <li>#Rows:{{nRows}}</li>
+        <li>#Mines:{{nMines}} </li>
+    </ol>
+</div>
+</template>
+<script>
+var cols, rows, mines;
+var minesweeper = require('minesweeper');
+cols = localStorage.getItem("cols");
+rows = localStorage.getItem('rows');
+mines = localStorage.getItem('mines');
+var mineArray = minesweeper.generateMineArray({
+    rows: 10,
+    cols: 10,
+    mines: 15
+});
+var board = new minesweeper.Board(mineArray);
+var grid = board.grid();
+var state = board.state();
+export default {
+    //name: "Board",
+    props: ['showGame'],
+    data: function data() {
+        return {
+            nCols: cols,
+            nRows: rows,
+            nMines: mines
+        }
+    }
+}
+</script>
+<style>
+.showme {
+    display: block;
+}
+</style>
